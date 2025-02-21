bde0d545b3ed6772874f91341d7e730f3b2b771c
Daniel Thompson <danielt3131@gmail.com>
2025-02-20 19:41:42 -0500

    Socket now closes when completed

--- server/src/main/java/io/github/danielt3131/cnt4504/server/Main.java
+++ server/src/main/java/io/github/danielt3131/cnt4504/server/Main.java
@@ -13,10 +13,13 @@ public class Main {
        try (ServerSocket serverSocket = new ServerSocket(2000)) {
            while (true) {
                Socket socket = serverSocket.accept();
+               System.out.println("New client connected");
                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                System.out.println(in.readLine());
                PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
                printWriter.println(new Date().toString());
+               printWriter.close();
+               socket.close();
            }
        } catch (IOException e) {
            throw new RuntimeException(e);
